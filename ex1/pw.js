d25280d37ced9fef205aeeb6df25319963e1cf54
Zach Freund <theeuskon@gmail.com>
2019-02-19 10:03:22 -0600

    Added new isMatch() to compare both PW fields

--- ex1/pw.js
+++ ex1/pw.js
@@ -1,4 +1,12 @@
 function isValidPW(){
-  let input = document.getElementById("pwField").value;
+  let input = document.getElementById("pwField1").value;
   console.log(input);
+}
+function isMatch(){
+  let lhs = document.getElementById("pwField1");
+  let rhs = document.getElementById("pwField2");
+  if(lhs === rhs){
+    return true;
+  }
+  return false;
 }

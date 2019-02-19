8962cc1a2d00c1800482c40f20c01610ccdee29b
Zach Freund <theeuskon@gmail.com>
2019-02-19 14:35:05 -0600

    added functionality for displaying error messages and removing them once they are finished

--- ex1/pw.js
+++ ex1/pw.js
@@ -1,10 +1,21 @@
 function isValidPW(){
-  if(isMatch() && lengthCheck()){
+  if(isMatch() && tooShort()){
+    let msg1 = document.getElementById("pwMatchError");
+    msg1.style.display = "none";
+    let msg2 = document.getElementById("pwLengthError");
+    msg2.style.display = "none";
     return true;
   }
   else{
-    //
-    alert("");
+    //return valid error message
+    if(!isMatch()){
+      let pwMatchMsg = document.getElementById("pwMatchError");
+      pwMatchMsg.style.display = "inline";
+    }
+    if(!tooShort()){
+      let tooShortMsg = document.getElementById("pwLengthError");
+      tooShortMsg.style.display = "inline";
+    }
   }
 }
 function isMatch(){
@@ -15,7 +26,8 @@ function isMatch(){
   }
   return false;
 }
-function lengthCheck(){
+//does it pass the length test?
+function tooShort(){
   let lhs = document.getElementById("pwField1").value;
   let rhs = document.getElementById("pwField2").value;
   if(lhs.length < 8){

4612063522aee7a4b0edaea9bd91f083cba3cacf
Zach Freund <z148f106@ku.edu>
2019-02-25 11:17:53 -0600

    made nextSlide() function into iterateSlide so it can take a param and use it to go next or prev.

--- ex2_Img_Slideshow/slideshow.js
+++ ex2_Img_Slideshow/slideshow.js
@@ -2,18 +2,18 @@ let imgIndex = 0;
 let slides = ["https://picsum.photos/200",
               "https://picsum.photos/201",
               "https://picsum.photos/202",
-              "https://picsum.photos/203"];
-function img_iterate(){
-
-}
-function nextSlide(n){
-
-  if ((imgIndex + n)> slides.length){
+              "https://ae01.alicdn.com/kf/HTB1HDKwSFXXXXb1XpXXq6xXFXXXs.jpg?size=110782&height=800&width=800&hash=d0f3c74ddb1ad6f68d90143dc1376c08"];
+function iterateSlide(n){
+  let nextIndex = imgIndex + n
+  if (nextIndex > (slides.length - 1)){
     imgIndex = 0;
+  } else if(nextIndex < 0){
+    imgIndex = slides.length - 1;
   } else{
-    imgIndex += n;
+    imgIndex = nextIndex;
   }
   //if (n < 0) {slideIndex = slides.length}
+  console.log(imgIndex);
   document.getElementById("myImg").src = slides[imgIndex];
 
 }
